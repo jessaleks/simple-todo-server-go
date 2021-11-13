aad3b1c8836b46a655ccff54f3f9bfb963388437
Aleksander Jess <jess.aleksander@gmail.com>
2021-11-13 23:24:42 +0100

    working on the server

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/jessaleks/simple-todo-server-go
 
 go 1.17
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.2 // indirect
+	github.com/mattn/go-sqlite3 v1.14.9 // indirect
+	gorm.io/driver/sqlite v1.2.4 // indirect
+	gorm.io/gorm v1.22.3 // indirect
+)
---
+++ models/todoModel.go
@@ -0,0 +1,48 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type Todo struct {
+	gorm.Model
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Completed   bool   `json:"completed"`
+}
+
+func (t *Todo) CreateTodo(db *gorm.DB) {
+	db.Create(&t)
+}
+
+func (t *Todo) GetTodo(db *gorm.DB, id uint) (Todo, error) {
+	var todo Todo
+	result := db.First(&todo, id)
+	if result.Error != nil {
+		return todo, result.Error
+	}
+	return todo, nil
+}
+
+func (t *Todo) GetTodos(db *gorm.DB, page int, pageSize int) ([]Todo, error) {
+	if pageSize <= 0 {
+		pageSize = 10
+	}
+	var todos []Todo
+	result := db.Find(&todos).Offset(page * pageSize).Limit(10)
+
+	if result.Error != nil {
+		return todos, result.Error
+	}
+	return todos, nil
+}
+
+func (t *Todo) UpdateTodo(db *gorm.DB) {
+	var todo Todo
+	db.Save(&todo)
+	return todo
+}
+
+func (t *Todo) DeleteTodo(db *gorm.DB) {
+	db.Delete(&t)
+}
